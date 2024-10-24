19e4ef9f1ff7780a0426e67591a92152af61a956
Alejandra Hernández Celestino <128764582+Alia-HC@users.noreply.github.com>
2024-10-23 18:52:31 -0600

    Parcial01 - Code for first evaluation (#1)
    Squash and merge:
    
    * Parcial01- Initial Configuration
    
    * Parcial01- Remove wrongly tracked files
    
    * Parcial01- Remove wrongly tracked files
    
    * Parcial01- First Entity
    
    * Parcial01- Add EntityFramework Core
    
    * Parcial01 - Add DataContext
    
    * Parcial01 - Add first EF migration
    
    * Parcial01 - Add database using dotnet ef database command
    
    * Parcial01 - Add Users controller
    
    * Parcial01 - Add Async code
    
    * Parcial01 - Initial Angular commit
    
    * Parcial01 - Add first Angular component
    
    * Parcial01 - Add CORS
    
    * Parcial01 - Add users list to html
    
    * Parcial01 - Add ngx-bootstrap@12 bootstrap font-awesome
    
    * Parcial01 - Create mkcert and add SSL
    
    * Parcial01 - Add password to appuser
    
    * Parcial01 - Add ApiControllerBase
    
    * Parcial01 - Add RegisterAsync
    
    * Parcial01 - Add DTOs
    
    * Parcial01 - Add login endpoint
    
    * Parcial01 - Add Token service
    
    Parcial01 - Add Token service
    
    * Parcial01 - Add authn middleware
    
    * Parcial01 - Add Token service
    
    * Parcial01 - Add authn middleware
    
    * Parcial01 - Add extension methods
    
    * Parcial01 - Add nav bar to Angular SPA
    
    * Parcial01 - Angular Template forms
    
    * Parcial01 - Inject account service into component
    
    * Parcial01 - Add conditionals to show/remove content from DOM
    
    * Parcial01 - Add bootstrap dropdown
    
    * Parcial01 - Persist the login
    
    * Parcial01 - Add home page
    
    * Parcial01 - Add register component
    
    * Parcial01 - PARENT to CHILD communication
    
    * Parcial01 - CHILD to PARENT communication
    
    * Parcial01 - Add user registration

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using System.Text;
+using API.Data;
+using API.Extensions;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors((cors) => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
