1596c5a5bfd60e57629bc2635852eec29b838b1a
alia-hc <ahcttino@gmail.com>
2024-10-27 20:52:47 -0600

    Parcial02 - Add new props to AppUser and create DateTime extension method

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,11 @@
+namespace API.Entities;
+
+public class Photo{
+    public int Id { get; set; }
+
+    public required string Url { get; set; }
+
+    public bool IsMain { get; set; }
+
+    public string? PblicId { get; set; }
+}
