337f5a98d679bd7abf2bf1fc9320a060d343e7c4
alia-hc <ahcttino@gmail.com>
2024-10-27 21:03:29 -0600

    Parcial02 - Add EF Relationships

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("Photos")]
 public class Photo{
     public int Id { get; set; }
 
@@ -8,4 +11,9 @@ public class Photo{
     public bool IsMain { get; set; }
 
     public string? PblicId { get; set; }
+
+    // EF Navigations Properties
+    //Para One-to-many relation
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; } = null!;
 }
